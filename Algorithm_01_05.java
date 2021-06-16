c1b82f14f26efe359a8b14d8b4a2b23627ed6270
cornarong <cornarong@gmail.com>
2021-06-16 23:36:54 +0900

    Algorithm_05~06

---
+++ Algorithm_01_05.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+// String str을 char배열로 받기 ==> str.toCharArray();
+// char배열을 String로 받기 ==> String.valueOf(char배열)
+// isLetterOrDigit : 특스문자 포함여부 확인 / 특수문자 없을 경우 true , 있을 경우 false
+// *point : lr:char배열의 첫번째 인덱스 , rt:char배열의 마지막값 인덱스 사용
+
+public class Algorithm_01_05 {
+
+    public String Solution(String str){
+        char[] chaArray = str.toCharArray();
+        int lt = 0;
+        int rt = str.length()-1;
+
+        while(lt < rt){
+            char temp = chaArray[lt];
+            if(Character.isLetterOrDigit(temp) == true &&
+                    Character.isLetterOrDigit(chaArray[rt]) == true) {
+                chaArray[lt] = chaArray[rt];
+                chaArray[rt] = temp;
+            }
+            lt ++;
+            rt --;
+        }
+        return String.valueOf(chaArray);
+    }
+    public static void main(String[] args) {
+        Algorithm_01_05 t = new Algorithm_01_05();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(t.Solution(str));
+    }
+}
---
+++ Algorithm_01_06.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+//  중복문자 제거
+//  for문으로 해당 값의 인덱스를 비교한다.
+//  indexOf(값)-> 첫번째로 나오는 값의 인덱스
+//  소스로 보는게 편함. 정리하면
+//  if(for의 i값이 str의 첫번째로 나오는 값의 인덱스번호랑 같으냐) 같으면 중복아니다.
+public class Algorithm_01_06 {
+
+    static void Solution(String str) {
+        String temp = "";
+        for(int i=0;i<str.length();i++){
+            if(i == str.indexOf(str.charAt(i))){
+                temp += str.charAt(i);
+            }
+        }
+        System.out.println(temp);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        Solution(str);
+    }
+}
