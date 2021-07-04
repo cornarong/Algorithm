bffb95f39778ba9a1c16ae6981e98eafc50154b4
cornarong <cornarong@gmail.com>
2021-07-04 23:08:57 +0900

    create new package - Algorithm06

---
+++ Algorithm06/Algorithm_06_01.java
@@ -0,0 +1,46 @@
+package Algorithm06;
+
+import java.util.Scanner;
+
+// ## 선택 정렬 ##
+// 선택정렬 : 기준이되는 수와 나머지수를 비교하여 가장 낮은수를 제일 앞으로 보내는 정렬
+
+// 선택 정렬은 첫 번째 자료를 두 번째 자료부터 마지막 자료까지 차례대로 비교하여 가장 작은 값을 찾아 첫 번째에 놓고,
+// 두 번째 자료를 세 번째 자료부터 마지막 자료까지와 차례대로 비교하여 그 중 가장 작은 값을 찾아 두 번째 위치에 놓는 과정을 반복하며 정렬을 수행한다.
+// 1회전을 수행하고 나면 가장 작은 값의 자료가 맨 앞에 오게 되므로 그 다음 회전에서는 두 번째 자료를 가지고 비교한다.
+// 마찬가지로 3회전에서는 세 번째 자료를 정렬한다.
+
+public class Algorithm_06_01 {
+
+    static void Solution(int n, int[] arr){
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                int temp = arr[i];
+                if(arr[i] > arr[j]){
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        for(int x : arr) System.out.printf(x+" ");
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Solution(n, arr);
+
+    }
+
+}
---
+++ Algorithm06/Algorithm_06_02.java
@@ -0,0 +1,52 @@
+package Algorithm06;
+
+import java.util.Scanner;
+
+// ## 버블 정렬 ##
+// 버블 정렬 : 서로 인접한 두 원소를 검사하여 정렬하는 알고리즘
+//           인접한 2개의 레코드를 비교하여 크기가 순서대로 되어 있지 않으면 서로 교환한다.
+//           선택 정렬과 기본 개념이 유사하다.
+
+public class Algorithm_06_02 {
+
+    static void Solution(int n, int[] arr){
+
+        // 방법 1
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+
+        // 방법 2
+        /*while (n != 2){
+            for (int j = 0; j < n-1; j++) {
+                int temp = arr[j];
+                if(arr[j] > arr[j+1]){
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+            n--;
+        }*/
+
+        for(int x : arr) System.out.printf(x+" ");
+
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Solution(n, arr);
+    }
+}
