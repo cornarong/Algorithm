f2034f3f7d74c8154bb25092417d59b8ba24ef48
cornarong <cornarong@gmail.com>
2021-07-29 00:14:00 +0900

    원더랜드(최소스패닝트리:프림 알고리즘, PriorityQueue)

--- algorithm09/Algorithm_09_08.java
+++ algorithm09/Algorithm_09_08.java
@@ -1,11 +1,17 @@
+package algorithm09;
+
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
 // 원더랜드(최소스패닝트리:프림 알고리즘,PriorityQueue)
+//최소 스패닝 트리의 정의는 그래프에서 그래프의 모든 정점을 연결하되,
+//사이클이 존재하지 않도록 모든 정점을 간선으로 연결하는 것을 의미한다.
+//이때, 간선의 가중치 합을 최소로 하며 연결하여야 한다.
+//이때 생성되는 최소 스패닝 트리는 무조건 하나의 그래프에서 하나만 생성된다고는 보장하지 못한다.
 
-public class Main {
-    
+public class Algorithm_09_08 {
+
     static class Edge implements Comparable<Edge>{
         int vertex;
         int cost;
