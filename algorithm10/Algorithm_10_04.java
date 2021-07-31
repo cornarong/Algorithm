ba5b729c178d286c9ab58603255210bc301e2e10
cornarong <cornarong@gmail.com>
2021-07-31 15:06:45 +0900

    가장 높은 탑 쌓기 (LIS 응용)

--- algorithm10/Algorithm_10_04.java
+++ algorithm10/Algorithm_10_04.java
@@ -1,4 +1,7 @@
+package algorithm10;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 // ## 가장 높은 탑 쌓기 (LIS 응용) ##
@@ -7,17 +10,22 @@ import java.util.Scanner;
 // * 초기값 셋팅
 // * 1씩 증가하면서 안으로는 1씩 감소하면서 비교 (해당 값 기준 앞 인덱스 전부 비교)
 
-class Point{
-    int a;
-    int h;
-    int w;
-    public Point(int a, int h, int w){
-        this.a = a;
-        this.h = h;
-        this.w = w;
+public class Algorithm_10_04 {
+
+    static class Point implements Comparable<Point> {
+        int a;
+        int h;
+        int w;
+        public Point(int a, int h, int w){
+            this.a = a;
+            this.h = h;
+            this.w = w;
+        }
+        @Override
+        public int compareTo(Point o) {
+            return this.a - o.a;
+        }
     }
-}
-public class Main {
 
     static int num;
     static int[] dy;
@@ -29,8 +37,10 @@ public class Main {
         for (int i = 1; i < num; i++) {
             int temp = arr.get(i).h;
             for (int j = i-1; j >= 0; j--) {
-                if(arr.get(j).a > arr.get(i).a && arr.get(j).w > arr.get(i).w){
-                    if(temp < dy[j] + arr.get(i).h) temp = dy[j] + arr.get(i).h;
+                if(arr.get(i).w > arr.get(j).w){
+                    if(temp < dy[j] + arr.get(i).h) {
+                        temp = dy[j] + arr.get(i).h;
+                    }
                 }
             }
             dy[i] = temp;
@@ -46,11 +56,12 @@ public class Main {
         ArrayList<Point> arr = new ArrayList<>();
         dy = new int[num];
         for (int i = 0; i < num; i++) {
-            int a = sc.nextInt();
-            int h = sc.nextInt();
-            int w = sc.nextInt();
+            int a = sc.nextInt(); // 넓이
+            int h = sc.nextInt(); // 높이
+            int w = sc.nextInt(); // 무게
             arr.add(new Point(a, h, w));
         }
+        Collections.sort(arr); // 넓이 기준 오름차순 정렬
         Solution(arr);
     }
 }
