1907e14f07a5ab6c1ce442855d6221d8ae674406
cornarong <cornarong@gmail.com>
2021-08-11 01:23:52 +0900

    k번째 수

--- programmers/Solution42748.java
+++ programmers/Solution42748.java
@@ -25,7 +25,7 @@ public class Solution42748 {
         return answer;
     }
 
-
+    // 예시 입출력1
     public static void main(String[] args) {
         int[] array = {1, 5, 2, 6, 3, 7, 4};
         int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
