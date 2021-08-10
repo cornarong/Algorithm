0b222eaccbccdcba0a1380ebfe5bfe5110541b55
cornarong <cornarong@gmail.com>
2021-08-11 01:27:26 +0900

    [2021 KAKAO BLIND RECRUITMENT] 신규 아이디 추천

--- programmers/Solution72410.java
+++ programmers/Solution72410.java
@@ -2,7 +2,7 @@ package programmers;
 
 import java.util.Locale;
 
-// [2021 KAKAO BLIND RECRUITMENT] 신규 아이디 추천
+// ## [2021 KAKAO BLIND RECRUITMENT] 신규 아이디 추천 ##
 // https://programmers.co.kr/learn/courses/30/lessons/72410
 public class Solution72410 {
 
