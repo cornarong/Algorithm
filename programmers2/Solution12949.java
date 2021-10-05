f2a10d554e44cc2aae6b97fbe95980ec96e215bc
cornarong <cornarong@gmail.com>
2021-10-06 00:08:33 +0900

    [연습문제] 행렬의 곱셈

---
+++ programmers2/Solution12949.java
@@ -0,0 +1,31 @@
+package programmers2;
+
+// ## [연습문제] 행렬의 곱셈 ##
+// https://programmers.co.kr/learn/courses/30/lessons/12949?language=java
+
+public class Solution12949 {
+
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        for(int i = 0 ; i < arr1.length ; ++i){
+            for(int j = 0 ; j < arr2[0].length ; ++j){
+                for(int k = 0 ; k < arr1[0].length ; ++k) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Solution12949 S = new Solution12949();
+        int[][] arr1 = {{1, 4}, {3, 2}, {4, 1}};
+        int[][] arr2 = {{3, 3}, {3, 3}};
+        int[][] answer = S.solution(arr1, arr2);
+        for (int i = 0; i < answer.length; i++) {
+            for (int j = 0; j < answer[i].length; j++) {
+                System.out.printf(answer[i][j]+" ");
+            }
+            if(i < answer.length-1) System.out.printf(", ");
+        }
+    }
+}
